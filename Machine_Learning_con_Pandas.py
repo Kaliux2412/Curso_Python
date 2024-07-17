8d8385cb579930e708229e09eabd52d39442aa73
Kaliux2412 <kgmoralesa24@gmail.com>
2024-07-17 16:57:40 -0600

    Cuarta Actualización

---
+++ Machine_Learning_con_Pandas.py
@@ -0,0 +1,9 @@
+#Otra forma de machine learning es GOOGLE COLAB
+#Numpy es una librería para trabajar en ciencia de datos y analisis númericos
+
+link_pandas = "https://colab.research.google.com/drive/1h3f32vBB5z2vmJI0wFTzPfJJQCk62NgX?usp=sharing"
+link_numpy = "https://colab.research.google.com/drive/1hTqSoLrKThf3GRyBOd08fQrGgwbedSEz?usp=sharing"
+link_matplotlib = "https://colab.research.google.com/drive/1LJ_I_fbNItwhlMAF3Ebev_ldO2VcUYnK?usp=sharing"
+#Programa de machine learning capaz de predecir quien sobrevivirá o no en el titaninc, de
+#acuerdo a una base de datos, para entregar graficos de arbol de desiciones y scatterplots.
+sobrevivir_al_titanic = "https://colab.research.google.com/drive/1hTqSoLrKThf3GRyBOd08fQrGgwbedSEz?usp=sharing"
